f944c9d16b0e30da4a6510fe56d6d7f2c45f5bcf
lnardon <lucaslnardon@gmail.com>
2023-05-05 16:46:26 -0300

    🎨 Misc

--- src/components/ProjectsTable/index.tsx
+++ src/components/ProjectsTable/index.tsx
@@ -1,7 +1,11 @@
 import ProjectCard from "../ProjectCard";
 import styles from "./styles.module.css";
 
-const ProjectsTable: React.FC<any> = ({ projects, handleProjectOpen }) => {
+const ProjectsTable: React.FC<any> = ({
+  projects,
+  handleProjectOpen,
+  handleProjectDeletion,
+}) => {
   return (
     <div className={styles.container}>
       {projects.map((project: any) => {
@@ -10,7 +14,7 @@ const ProjectsTable: React.FC<any> = ({ projects, handleProjectOpen }) => {
             name={project.name}
             key={project.name}
             handleClick={() => handleProjectOpen(project)}
-            handleDelete={() => alert(project.name)}
+            handleDelete={() => handleProjectDeletion(project.id)}
           />
         );
       })}
