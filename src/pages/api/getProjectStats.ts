2531c561ad7f16bb133e1dcb0acae3841ae0194a
lnardon <lucaslnardon@gmail.com>
2023-06-25 05:42:53 -0300

    ♻️ Update stats endpoint

--- src/pages/api/getProjectStats.ts
+++ src/pages/api/getProjectStats.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../lib/prisma";
 
-export default async function getStats(
+export default async function getProjectStats(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
@@ -39,10 +39,25 @@ export default async function getStats(
         },
       });
 
+      let thirtyDaysAgo = new Date();
+      thirtyDaysAgo.setDate(sevenDaysAgo.getDate() - 30);
+      const last_30_days = await prisma.entry.aggregate({
+        _sum: {
+          time: true,
+        },
+        where: {
+          projectId: projectId,
+          createdAt: {
+            gte: thirtyDaysAgo,
+          },
+        },
+      });
+
       await prisma.$disconnect();
       res.status(200).json({
         total_time: total_time._sum.time,
         last_7_days: last_7_days._sum.time,
+        last_30_days: last_30_days._sum.time,
       });
     } catch (error) {
       res
